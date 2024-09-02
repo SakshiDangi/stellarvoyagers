9c02a5244ebd9680e91f9eef75b46f4b6f3f3f2a
sakshi <learnogo@gmail.com>
2024-09-02 09:32:19 -0700

    connected with mongodb, created blogPost, createPost, addPost details and fetch data without an API. created dynamic singleBlog Post page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.scss";
+import { getPosts } from "@/lib/data";
 
-export default function Blog() {
+// FETCH DATA WITH AN API
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const Blog = async () => {
+
+  // FETCH DATA WITH AN API
+  // const posts = await getData();
+
+  // FETCH DATA WITHOUT AN API
+  const posts = await getPosts();
+
   return (
-    <div>Blog</div>
-  )
-}
+    <div className={styles.container}>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import styles from "./postCard.module.scss"
+import Link from "next/link"
+
+export default function postCard({post}) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        {post.img && <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img}/>
+        </div>}
+        <span className={styles.date}>{post.createdAt?.toString().slice(4, 16)}</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
+      </div>
+    </div>
+  )
+}
