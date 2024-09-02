736f3461f90eefb90e59ac9b0806e4ca2fa9ec89
sakshi <learnogo@gmail.com>
2024-09-02 06:56:24 -0700

    styling frontend dashboard and started working on server side

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.logo}>StellaVoyagers</div>
+      <div className={styles.text}>
+        StellaVoyagers creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
+};
+
+
+
