de3e865b452fc9779d0197f45fee8cafa29e46dc
sakshi <learnogo@gmail.com>
2024-09-04 23:48:54 -0700

    password removed

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -10,7 +10,6 @@ const LoginForm = () => {
 
   return (
     <form className={styles.form} action={formAction}>
-      <p>For Admin username: test1 & password: test123</p>
       <input type="text" placeholder="username" name="username" />
       <input type="password" placeholder="password" name="password" />
       <button>Login</button>
