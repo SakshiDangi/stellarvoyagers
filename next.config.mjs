1e00572307110e800bb79057daa05229c304ba96
sakshi <learnogo@gmail.com>
2024-09-05 08:35:07 -0700

    next.config.mjs deployment issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-          {
-            protocol: 'https',
-            hostname: "img.freepik.com"
-          }
-        ]
-    }
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://stellarvoyagers.vercel.app/:path*',
+      },
+    ]
+  },
+
+  images:{
+      remotePatterns:[
+        {
+          protocol: 'https',
+          hostname: "img.freepik.com"
+        }
+      ]
+  }
 };
 
 export default nextConfig;
